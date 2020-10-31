6c997cacd87f059c912b4afe365dd3f152692c2d
Mohammad-Javad Rezaei <73162356+javadrezaei7@users.noreply.github.com>
2020-10-31 20:09:59 +0330

    Update main.py

--- codes/main.py
+++ codes/main.py
@@ -5,7 +5,7 @@ from conf_api import ConfApi
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-C', "--configaddr", dest='configaddr', help="Address of config file.", type=str,
-                        default='/home/javad/PycharmProjects/apk/config.json')
+                        default='config.json')
     # parser.add_argument('-P', "--port", dest='port', help="Syslog port.", type=int, default=514)
     parser.add_argument('-H', "--host", dest='host', help="Remote syslog server ip.", type=str, default="127.0.0.1")
     args = parser.parse_args()
